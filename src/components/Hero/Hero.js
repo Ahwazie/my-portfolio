31b2abaeaef7edf4cb7187a060c114a356069bf9
Ahwazie <wazie8699@gmail.com>
2024-03-17 03:08:42 +0800

    Updated contact button and fixed mobile view page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,6 +2,19 @@ import React from 'react';
 import './Hero.css';
 
 function Hero() {
+  // Function to handle smooth scroll
+  const handleNavClick = (e) => {
+    e.preventDefault(); // Prevent default anchor behavior
+    const targetId = e.currentTarget.getAttribute('href').substring(1); // Get the target ID (removing the '#')
+    const targetElement = document.getElementById(targetId); // Find the target element
+
+    // Smooth scroll to the target element
+    window.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: 'smooth'
+    });
+  };
+
   return (
     <div className="hero">
       <h2 className="hero-heading">Hello, I'm </h2>
@@ -9,10 +22,12 @@ function Hero() {
       <p className="hero-title"></p>
       <div className="hero-buttons">
         <a href="/path-to-cv" className="btn download-cv">Download CV</a>
-        <a href="/contact-info" className="btn contact-info">Contact Info</a>
+        {/* Update the href to point to #contactsection and add onClick handler */}
+        <a href="#contactsection" className="btn contact-info" onClick={handleNavClick}>Contact Info</a>
       </div>
     </div>
   );
 }
 
 export default Hero;
+
